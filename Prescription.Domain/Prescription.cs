9d947840138b49bf8954c1d97c1fb849989f8ca0
Jamir Castro Araújo <jamir.c.a@hotmail.com>
2018-03-27 23:12:30 -0300

    Add project files.

---
+++ Prescription.Domain/Prescription.cs
@@ -0,0 +1,14 @@
+﻿using EventFlow.Aggregates;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prescription.Domain
+{
+    public class Prescription : AggregateRoot<Prescription, PrescriptionId>
+    {
+        protected Prescription(PrescriptionId id) : base(id)
+        {
+        }
+    }
+}
